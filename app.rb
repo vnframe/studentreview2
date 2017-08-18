3d5ed3308931a293714329c0eaa7141d02049849
vnframe <vnframe95@icloud.com>
2017-08-18 19:44:18 -0400

    checkboxes working but returning EVERYTHING

--- app.rb
+++ app.rb
@@ -5,15 +5,19 @@ get '/' do
 end
 
 post "/sandwich_parts" do
- topping = params[:topping]
+  topping = params[:topping]
   topping = topping.to_s
 redirect "/confirmation?topping=" + topping
 
 end
 get "/confirmation" do
-  topping = params[:topping]
-  topping = topping.split("")
+  topping = params[:topping].split
+  # topping = topping.split
   puts topping
   #topping = topping.to_a
   erb :confirmation, :locals => {:topping => topping}
-  end
+end
+post "/confirmation" do
+  topping = params[:topping].to_s
+  redirect "/results?topping=" + topping
+end
