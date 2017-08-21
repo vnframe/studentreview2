f20ace8267fa9976126573d95e220342d1c40bb6
vnframe <vnframe95@icloud.com>
2017-08-21 10:00:59 -0400

    done

--- app.rb
+++ app.rb
@@ -5,19 +5,31 @@ get '/' do
 end
 
 post "/sandwich_parts" do
-  topping = params[:topping]
+  puts "MADE IT TO SANDWICH_PARTS post"
+  topping = params[:topping].join(',')
   topping = topping.to_s
+  puts "topping is #{topping}"
 redirect "/confirmation?topping=" + topping
-
 end
+
 get "/confirmation" do
-  topping = params[:topping].split
-  # topping = topping.split
-  puts topping
-  #topping = topping.to_a
+puts "MADE IT TO GET CONFIRM"
+  topping = params[:topping].split(',')
+  puts "Topping is #{topping}"
+
   erb :confirmation, :locals => {:topping => topping}
 end
 post "/confirmation" do
-  topping = params[:topping].to_s
-  redirect "/results?topping=" + topping
+  puts "MADE IT TO POST CONFIRMATION"
+  topping = params[:topping].join(',')
+  puts "topping is #{topping}"
+  redirect "/end?topping=" + topping 
+end
+
+get "/end" do 
+  topping = params[:topping].split(',')
+  erb :end, :locals => {:topping => topping}
+end
+post "/end" do
+  topping = params[:topping].join(',')
 end
